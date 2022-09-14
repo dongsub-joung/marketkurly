ece38a08716447ab930b8fd69fde0fcb18aed715
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-09-15 00:29:44 +0900

    apart user

--- src/main/java/com/example/marketkurly/model/Cart.java
+++ src/main/java/com/example/marketkurly/model/Cart.java
@@ -31,12 +31,12 @@ public class Cart {
     private List<Long> productIds;
 
 
-//    @ManyToOne//(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user_Id")
-//    private User user;
+    // @ManyToOne//(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "user_Id")
+    // private User user;
 
-    @OneToOne
-    private User user;
+    // @OneToOne
+    // private User user;
 
 
     public Cart(RequestCartDto chartDto) {
